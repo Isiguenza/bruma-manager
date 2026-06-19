d1efbcf3b185fc1d3e6134ea805953769bb188d5
isiguenza <isiguenzan@gmail.com>
2026-06-19 14:47:29 -0600

    loyalty public

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ export default async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Public routes that don't need authentication
-  const publicRoutes = ['/login', '/bar', '/dispatch', '/orders'];
+  const publicRoutes = ['/login', '/bar', '/dispatch', '/orders', '/loyalty'];
   const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
   
   if (isPublicRoute) {
